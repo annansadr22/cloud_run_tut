5fa8757b586485961bcdc0a2a9f2bbd0cd186fa3
annansadr22 <126930908+annansadr22@users.noreply.github.com>
2023-11-14 17:17:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from google.cloud import bigquery
 def hello_world():
     """Example Hello World route."""
 
-    return f"Hello World!!!ANavdeep"
+    return f"Hello World!!!ZARA"
 
 @app.route("/event_looks", methods=['POST'])
 def event_looks():
